96f337c924747141d9864537294d24d738bf030e
silfigabrilia <66557176+silfigabrilia@users.noreply.github.com>
2021-04-12 15:13:01 +0700

    Minggu10

---
+++ Minggu10/CodeIgniter/application/views/view_teslib.php
@@ -0,0 +1,27 @@
+<?php 
+$template = array( 
+    'table_open'=> '<table border="1 cellpadding="4" cellspacing="0">', 
+    'table_close'=> '</table>' 
+); 
+    $this->table->set_template($template); 
+    $this->table->set_heading(array('No', 'Nama', 'Prodi')); 
+    $num = 1; 
+    
+    foreach($Mahasiswa as $row) 
+    { 
+        $this->table->add_row(array($num,$row['nama'],$row['prodi'])); 
+        $num++; 
+        } 
+        echo $this->table->generate(); 
+
+$this->benchmark->mark('code_start');
+// Some code happens here
+$this->benchmark->mark('code_end');
+echo $this->benchmark->elapsed_time('code_start', 'code_end');
+
+// total execution time
+echo $this->benchmark->elapsed_time();
+
+// memory consumption
+echo $this->benchmark->memory_usage();
+?>
